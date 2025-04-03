26bc32d779be1f7bdbc05c94aafca7a60630a269
Nicole Smith <nicolealeisha@gmail.com>
2025-04-04 10:59:17 +1300

    front end file structure expanded

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from '../styles/Header.module.css';
+
+function Header () {
+    return ( 
+        <>
+        
+        </>
+
+     );
+}
+
+export default Header ;
---
+++ frontend/src/pages/Listing.jsx
@@ -0,0 +1,11 @@
+import styles from '.../styles/listing.module.css';
+
+function Listing () {
+    return (  
+        <>
+        
+        </>
+    );
+}
+
+export default Listing ;
