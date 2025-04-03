f70fa6625411fb2f06dd84bf031aa47d6cdaed39
Nicole Smith <nicolealeisha@gmail.com>
2025-04-04 10:30:26 +1300

    backend folder structure added

---
+++ backend/models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+// Product Schema
+const productSchema = mongoose.Schema({
+    title: {type: String},
+    description: {type: String},
+    start_price: {type: Number},
+    reserve_price: {type: Number},
+    image: {type: String},
+    category: {type: String},
+    location: {type: String},
+    condition: {type: String},
+    auction_start_date: {type: Date},
+    auction_start_time: {type: String},
+    auction_end_date: {type: Date},
+    auction_end_time: {type: String},
+});
+
+// define and export
+module.exports = mongoose.model('Product', productSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const express = require('express');
-// const productRoutes = require('./routes/productRoutes');
+const productListingRoute = require('./routes/productListing');
 
 const app = express();
 app.use(express.json()); 
@@ -9,10 +9,24 @@ app.use(express.json());
 mongoose.Promise = global.Promise;
 
 // Connect to the database using ipv4 address specifically
-// explicitly set the server selection timeout to 30 seconds to avoid the error "Server selection timed out after 30000 ms"
+// team to confirm their connection string is correct
 mongoose.connect('mongodb://127.0.0.1:27017/mission-05', {
     serverSelectionTimeoutMS: 30000,
+    // explicitly set the server selection timeout to 30 seconds to avoid the error "Server selection timed out after 30000 ms"
 })
+    .then(() => {
+        console.log("Connected to MongoDB successfully!");
+    })
     .catch(err => {
         console.error("Error connecting to MongoDB:", err);
     });
+
+// Import model --  required here??
+const Product = require('./models/product');
+
+// Set up routes
+app.use(productListingRoute);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
