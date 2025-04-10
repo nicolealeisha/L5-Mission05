d8ec4401ce6477cbd5ae4e886a1dccaafa41a7ba
Nicole Smith <nicolealeisha@gmail.com>
2025-04-10 13:43:38 +1200

    listing lh column additions

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -7,6 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircleUser } from '@fortawesome/free-regular-svg-icons';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import { faHeartCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { faShareNodes } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 import ListingPayment from '../components/ListingPayment';
 import ListingQuestions from '../components/ListingQuestions';
 import ListingRHside from '../components/ListingRHSide';
@@ -17,7 +19,7 @@ function Listing() {
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
+    
     // Fetch listing data from the backend using the listingId
     useEffect(() => {
         const fetchListing = async () => {
@@ -56,22 +58,12 @@ function Listing() {
             <div className={styles.listingContainer}>
                 {listing ? (
                     <>
-                    <div className={styles.topListingDetails}>
-   
                         <div className={styles.listingLHside}>
                             <div className={styles.imgs}>
                                 <img src={listing.image} alt={listing.title} className={styles.listingImage} />
                                 <img src='/images/watchlist-btn.png' alt='Watchlist' className={styles.watchlist} />
                             </div>
-                        </div>
 
-                        <div className={styles.listingRHside}>
-                            <ListingRHside />
-                        </div>
-                    </div>
-                    <div className={styles.btmListingDetails}>
-                        
-                        <div className={styles.listingLHside}>
                             <h2>Details</h2>
                             <p className={styles.description}>Condition: {listing.condition}</p>
                             <h2>Full Description</h2>
@@ -82,8 +74,8 @@ function Listing() {
                             <ListingQuestions />
 
                             <div className={styles.sellerDetails}>
+                                <h2>About the seller</h2>
                                 <div className={styles.sellerDetailsCentre}>
-                                    <h2>About the seller</h2>
                                     <div className={styles.sellerAvatar}>
                                         <FontAwesomeIcon icon={faCircleUser} className={`${styles.faIcon} ${styles.sellerAvatarIcon}`} />
                                     </div>
@@ -97,10 +89,27 @@ function Listing() {
                                     <button className={`${styles.blueBtn} ${styles.sellerBtn}`}><FontAwesomeIcon icon={faHeartCirclePlus} className={styles.favouritesIcon}/>Add to Favourite Sellers</button>
                                     <a href='/404'>Read our safe buying guide</a>
                                 </div>
-                            </div>   
+                            </div> 
+
+                            <div className={styles.shareListing}>
+                                <a className={styles.share} ><FontAwesomeIcon icon={faShareNodes} className={styles.shareIcon}/> Share this listing</a>
+                                <span className={styles.shareDot}>•</span>
+                                <span className={styles.shareInfo}>Listing #{listing._id}</span>
+                                <span className={styles.shareDot}>•</span>
+                                <span className={styles.shareInfo}>Page views: 40</span>
+                            </div>  
+                            <div className={styles.communityWatchInfo}>
+                                <span className={styles.communityWatch}><FontAwesomeIcon icon={faStar} className={styles.commWatchIcon} /> Community Watch:</span>
+                                <a className={styles.commWatchReport}>Report this listing</a>
+                            </div>
+                            
                         </div>
 
-                    </div>
+                        <div className={styles.listingRHside}>
+                            <ListingRHside />
+                        </div>
+
+
                     </>
                 ) : (
                     <div>No listing data available</div>
