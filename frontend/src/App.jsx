03c50f431dcc60b8e6bf8270c16096f4c57afc88
Nicole Smith <nicolealeisha@gmail.com>
2025-05-03 10:26:06 +1200

    post mission additions for portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,11 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Listing from './pages/Listing';
 import SearchResults from './pages/SearchResults';
+import Home from './pages/Home';
+import Browse from './pages/Browse';
+import "@fontsource/barlow-condensed"; // Defaults to weight 400
+import "@fontsource/barlow-condensed/400.css"; // Specify weight
+import "@fontsource/barlow-condensed/400-italic.css"; 
 
 function App() {
   
@@ -14,12 +19,15 @@ function App() {
 
         {/* Routes defined here */}
         <Routes>
-          <Route path='/' element={<h1>Home Page</h1>} /> {/* Home page */}
+          <Route path='/' element={<Home />} /> {/* Home page */}
           <Route path='/404' element={<h1>404 Page Not Found</h1>} /> {/* 404 page */}
           <Route
             path="/marketplace/:category/:subcategory/listing/:listingId"
             element={<Listing />}
           />
+          <Route path="/marketplace" element={<Browse />} />
+          <Route path="/marketplace/:categoryName" element={<Browse />} />
+          <Route path="/marketplace/:categoryName/:subcategoryName" element={<Browse />} /> 
           <Route
             path="/search/:kw"
             element={<SearchResults />}
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,88 +7,91 @@ function Footer() {
                 <div className={styles.orange_line}> </div>
                 <nav>
                     <div className={styles.firstLineFooter}>
-                        <a href="/#doyoulikefishsticks?" className={styles.logo}><img src="/images/logo.png" alt="Trade Me Logo" title="Trade Me" /></a>
-                        <a href="/#doyoulikefishsticks?">List an item</a>
-                        <a href="/#doyoulikefishsticks?">Watchlist</a>
-                        <a href="/#doyoulikefishsticks?">Favourites</a>
-                        <a href="/#doyoulikefishsticks?">My Trade Me</a>
-                        <a href="/#doyoulikefishsticks?">Log out</a>
+                        <a className={`${styles.navItem} ${styles.logo} ${styles.footerHeader}`} href="/#doyoulikefishsticks?"><img src="/images/logo.png" alt="Trade Me Logo" title="Trade Me" /></a>
+                        <a className={`${styles.navItem} ${styles.footerHeader}`} href="/#doyoulikefishsticks?">List an item</a>
+                        <a className={`${styles.navItem} ${styles.footerHeader}`} href="/#doyoulikefishsticks?">Watchlist</a>
+                        <a className={`${styles.navItem} ${styles.footerHeader}`} href="/#doyoulikefishsticks?">Favourites</a>
+                        <a className={`${styles.navItem} ${styles.footerHeader}`} href="/#doyoulikefishsticks?">My Trade Me</a>
+                        <a className={`${styles.navItem} ${styles.footerHeader}`} href="/#doyoulikefishsticks?">Log out</a>
                     </div>
-                </nav>{/* *******  THE FIRST BLOCK IS HORIZONTAL. THE NEXT 6 MAKE UP THE 6 COLUMNS */}
+                </nav>
 
-
+                <div className={styles.secGrid}>
                 <nav className={styles.gridRow} id={styles["box1"]}>
                     <div className={styles.gridCol}>
-                        <a href="/#doyoulikefishsticks?">Marketplace</a>
-                        <a href="/#doyoulikefishsticks?">Latest deals</a>
-                        <a href="/#doyoulikefishsticks?">Stores</a>
-                        <a href="/#doyoulikefishsticks?">Closing soon</a>
-                        <a href="/#doyoulikefishsticks?">$1 reserve</a>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav1}`} href="/#doyoulikefishsticks?">Marketplace</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Latest deals</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Stores</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Closing soon</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">$1 reserve</a>
                     </div>
                 </nav>
 
 
                 <nav className={styles.gridRow} id={styles["box2"]}>
                     <div className={styles.gridCol}>
-                        <a href="/#">Jobs</a>
-                        <a href="/#">Browse categories</a>
-                        <a href="/#">Careers advice</a>
-                        <a href="/#">JobSmart</a>
-                        <a href="/#">Advertisers advice</a>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav2}`} href="/#">Jobs</a>
+                        <a className={styles.navItem} href="/#">Browse categories</a>
+                        <a className={styles.navItem} href="/#">Careers advice</a>
+                        <a className={styles.navItem} href="/#">JobSmart</a>
+                        <a className={styles.navItem} href="/#">Advertisers advice</a>
                     </div>
                 </nav>
 
 
                 <nav className={styles.gridRow} id={styles["box3"]}>
                     <div className={styles.gridCol}>
-                        <a href="/#">Motors</a>
-                        <a href="/#">Browse all cars</a>
-                        <a href="/#">Other vehicles</a>
-                        <a href="/#">Buying & Selling</a>
-                        <a href="/#">Dealer news & info</a>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav3}`} href="/#">Motors</a>
+                        <a className={styles.navItem} href="/#">Browse all cars</a>
+                        <a className={styles.navItem} href="/#">Other vehicles</a>
+                        <a className={styles.navItem} href="/#">Buying & Selling</a>
+                        <a className={styles.navItem} href="/#">Dealer news & info</a>
                     </div>
                 </nav>
 
                 <nav className={styles.gridRow} id={styles["box4"]}>
                     <div className={styles.gridCol}>
-                        <a href="/#">Property</a>
-                        <a href="/#">International property</a>
-                        <a href="/#">News & guides</a>
-                        <a href="/#">Sold Properties</a>
-                        <a href="/#">OneHub for agents</a>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav4}`} href="/#">Property</a>
+                        <a className={styles.navItem} href="/#">International property</a>
+                        <a className={styles.navItem} href="/#">News & guides</a>
+                        <a className={styles.navItem} href="/#">Sold Properties</a>
+                        <a className={styles.navItem} href="/#">OneHub for agents</a>
                     </div>
                 </nav>
 
                 <nav className={styles.gridRow} id={styles["box5"]}>
                     <div className={styles.gridCol}>
-                        <a href="/#">Services</a>
-                        <a href="/#">Trades</a>
-                        <a href="/#">Domestic services</a>
-                        <a href="/#">Events & entertainment</a>
-                        <a href="/#">Health & wellbeing</a>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav5}`} href="/#">Services</a>
+                        <a className={styles.navItem} href="/#">Trades</a>
+                        <a className={styles.navItem} href="/#">Domestic services</a>
+                        <a className={styles.navItem} href="/#">Events & entertainment</a>
+                        <a className={styles.navItem} href="/#">Health & wellbeing</a>
                     </div>
                 </nav>
 
                 <nav className={styles.gridRow} id={styles["box6"]}>
-                    <div className="grid-col navItem">
-                        <a href="/#">Community</a>
-                        <a href="/#">Help</a>
-                        <a href="/#">Announcements</a>
-                        <a href="/#">Trust & safety</a>
-                        <a href="/#">Seller information</a>
+                    <div className={styles.gridCol}>
+                        <a className={`${styles.navItem} ${styles.firstNavItem} ${styles.nav6}`} href="/#">Community</a>
+                        <a className={styles.navItem} href="/#">Help</a>
+                        <a className={styles.navItem} href="/#">Announcements</a>
+                        <a className={styles.navItem} href="/#">Trust & safety</a>
+                        <a className={styles.navItem} href="/#">Seller information</a>
                     </div>
                 </nav>
+                </div> 
 
-                <nav className={styles.gridRow}>
+                <div className={styles.mobileFooter}>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Desktop site</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Help</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Contact us</a>
+                        <a className={styles.navItem} href="/#doyoulikefishsticks?">Terms & Conditions</a>
+                </div>
+
+                <div className={styles.finalGridRow}>
                     <div className={styles.gridCol}>
-                        <p>                            © 2025 Trade Me Limited </p>
+                        <p>© 2025 Trade Me Limited </p>
                     </div>
-                    <div className={styles.gridCol}>
-                        <p>
-                            Desktop site About Us Careers Advertise Privacy policy Terms & conditions Contact Us
-                        </p>
-                    </div>
-                </nav>
+                </div>
             </footer >
         </>
     );
